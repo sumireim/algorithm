961843da0c3ae37cc6aade5aff4b7c3106b8886e
23imamoto <sumire@g.ecc.u-tokyo.ac.jp>
2025-06-03 17:59:15 +0900

    basic6

--- day6/basic_6_b.py
+++ day6/basic_6_b.py
@@ -19,13 +19,11 @@ def RollingHashMatch(text, pattern):
     for i in range(p_len):
         # i番目まで： 𝑎
         p_hash = (p_hash + ord(pattern[i]) * powers[i]) % h
-       
         # i-1番目まで： 𝑎
         t_hash = (t_hash + ord(text[i]) * powers[i]) % h
     
     result = []
 
-    # 照合
     for i in range(t_len - p_len + 1):
         if p_hash == t_hash:
             if text[i:i+p_len] == pattern:
@@ -34,25 +32,14 @@ def RollingHashMatch(text, pattern):
         if i + p_len < t_len:
             # ローリングで次のハッシュを計算
             left = ord(text[i]) * powers[0] % h
-            t_hash = (t_hash - left + h) % h  # 左端削除
-            t_hash = (t_hash * base) % h         # 右に一つずらす（base倍）
-            t_hash = (t_hash + ord(text[i + p_len])) % h  # 右端追加
+            t_hash = (t_hash - left + h) % h 
+            t_hash = (t_hash * base) % h 
+            t_hash = (t_hash + ord(text[i + p_len])) % h 
             
     return result
-    # 一番先頭でマッチしていればそこで終わり
-    #if t_hash == p_hash:
-    #    return 0
 
-def main(s, t):
-    # このコードは標準入力と標準出力を用いたサンプルコードです。
-    # このコードは好きなように編集・削除してもらって構いません。
-    result = RollingHashMatch(s, t)
-    return result
-
-
-if __name__ == '__main__':
-    input = sys.stdin.readline
-    s = input().strip()
-    t = input().strip()
-    result = main(s, t)
-    print(result)
+input = sys.stdin.readline
+s = input().strip()
+t = input().strip()
+result = RollingHashMatch(s, t)
+print(result)
